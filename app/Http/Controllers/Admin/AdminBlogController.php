d188c1dfea5223fc1e58f8ffe33abe3db5ed266d
motoki kounosu <kounotori100588@i.softbank.jp>
2025-07-23 22:51:43 +0900

    section13～14 削除まで終了

---
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\StoreBlogRequest;
+use App\Http\Requests\Admin\UpdateBlogRequest;
+use App\Models\Blog;
+use DragonCode\Contracts\Cache\Store;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\Request;
+
+class AdminBlogController extends Controller
+{
+	/**
+	 * ブログ一覧画面
+	 */
+	public function index()
+	{
+		$blogs = Blog::all();
+		return view('admin.blogs.index', ['blogs' => $blogs]);
+	}
+
+	/**
+	 * ブログ投稿機能
+	 */
+	public function create()
+	{
+		return view('admin.blogs.create');
+	}
+
+	/**
+	 * ブログ投稿処理
+	 * 
+	 * @param  StoreBlogRequest  $request
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
+	public function store(StoreBlogRequest $request)
+	{
+		$saveImagePath = $request->file('image')->store('blogs', 'public');
+		$blog = new Blog($request->validated());
+		$blog->image = $saveImagePath;
+		$blog->save();
+
+		return to_route('admin.blogs.index')->with('success', 'ブログを投稿しました');
+	}
+
+	/**
+	 * Display the specified resource.
+	 */
+	public function show(string $id)
+	{
+		//
+	}
+
+	/**
+	 * 指定したIDのブログ編集画面
+	 */
+	public function edit(string $id)
+	{
+		$blog = Blog::findOrFail($id);
+		return view('admin.blogs.edit', ['blog' => $blog]);
+	}
+
+	/**
+	 * 指定したIDのブログ更新処理
+	 * 
+	 * @param  UpdateBlogRequest  $request
+	 */
+	public function update(UpdateBlogRequest $request, string $id)
+	{
+		$blog = Blog::findOrFail($id);
+		$updateDate = $request->validated();
+
+		// 画像を変更する場合
+		if ($request->has('image')) {
+			// 変更前の画像を削除
+			Storage::disk('public')->delete($blog->image);
+			// 変更後の画像をアップロード、保存パスを更新対象データにセット
+			$updateDate['image'] = $request->file('image')->store('blogs', 'public');
+		}
+		$blog->update($updateDate);
+
+		return to_route('admin.blogs.index')->with('success', 'ブログを更新しました');
+	}
+
+	/**
+	 * 指定したIDのブログの削除処理
+	 */
+	public function destroy(string $id)
+	{
+		$blog = Blog::findOrFail($id);
+		$blog->delete();
+		Storage::disk('public')->delete($blog->image);
+
+		return to_route('admin.blogs.index')->with('success', 'ブログを削除しました');
+	}
+}
